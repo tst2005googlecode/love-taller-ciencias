361868d3fd97d22bd5d7c2d84f9df760a946ebbd
fernando.bugni@gmail.com <fernando.bugni@gmail.com@a856b7b3-8955-f75e-7849-990cd1db449c>
2012-08-12 14:39:08 +0000

    Ahora anda decentemente :)

--- trunk/juego/main.lua
+++ trunk/juego/main.lua
@@ -9,10 +9,10 @@ function love.load()
     skin135 = love.graphics.newImage("images/plane1/135.png")
     skin150 = love.graphics.newImage("images/plane1/150.png")
     skinPlane = { skin30, skin75, skin90, skin135, skin150}
-    plane1 = { x = 200, y = 200, rot = 0, rotH = 90, skin = skinPlane}
+    plane1 = { x = 500, y = 500, rot = 0, rotH = 90, skin = skinPlane}
 
     --Variables globales
-    delta_rotH = 10
+    delta_rotH = 1.8
 
     width = skin30:getWidth()
     height = skin30:getHeight()
@@ -25,24 +25,13 @@ function love.draw()
     love.graphics.print(delta_x, 200, 0)
     love.graphics.print(delta_y, 350, 0)
 
-    love.graphics.draw(actualSkin, plane1.x, plane1.y, plane1.rotH*0.1, 1, 1, width / 2, height / 2)    
+    love.graphics.draw(actualSkin, plane1.x, plane1.y, -(plane1.rotH-90)*0.1, 1, 1, width / 2, height / 2)    
 end
 
 function love.update()
 
     actualSkin = plane1.skin[3]
-    -- if plane1.rotH <= 30 then
-    --     actualSkin = plane1.skin[1]
-    -- elseif plane1.rotH <= 75 then
-    --     actualSkin = plane1.skin[2]
-    -- elseif plane1.rotH <= 90 then
-    --     actualSkin = plane1.skin[3]
-    -- elseif plane1.rotH <= 135 then
-    --     actualSkin = plane1.skin[4]
-    -- else 
-    --     actualSkin = plane1.skin[5]
-    -- end
-
+    
     delta_x = math.sin((plane1.rotH - 90) * 0.1)
     delta_y = math.cos((plane1.rotH - 90) * 0.1)
 
@@ -51,17 +40,11 @@ function love.update()
 
     if love.keyboard.isDown("a") then
         plane1.rotH = plane1.rotH - delta_rotH
+        actualSkin = plane1.skin[4]
     end
 
     if love.keyboard.isDown("d") then
         plane1.rotH = plane1.rotH + delta_rotH
-    end
-
-    if plane1.rotH < 0 then
-        plane1.rotH = 0
-    end
-
-    if plane1.rotH > 200 then
-        plane1.rotH = 200
+        actualSkin = plane1.skin[2]
     end
 end
--- trunk/juego/make.sh
+++ trunk/juego/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip juego.zip * -r
+zip juego.zip main.lua images/map.jpg images/plane1/30.png images/plane1/75.png images/plane1/90.png images/plane1/135.png images/plane1/150.png -r 
 mv juego.zip juego.love
