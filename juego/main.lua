8304861159e72edd97d853a0b1b431c3493d503d
fernando.bugni@gmail.com <fernando.bugni@gmail.com@a856b7b3-8955-f75e-7849-990cd1db449c>
2012-08-20 21:57:03 +0000

    Tira rayos agregado

--- juego/main.lua
+++ juego/main.lua
@@ -1,21 +1,22 @@
+require "table"
 
 --Esto representa a inicializar
 function love.load()
     map = love.graphics.newImage("images/map.jpg")
 
-    skin30 = love.graphics.newImage("images/plane1/30.png")
     skin75 = love.graphics.newImage("images/plane1/75.png")
     skin90 = love.graphics.newImage("images/plane1/90.png")
     skin135 = love.graphics.newImage("images/plane1/135.png")
-    skin150 = love.graphics.newImage("images/plane1/150.png")
-    skinPlane = { skin30, skin75, skin90, skin135, skin150}
+    skinPlane = { skin75, skin90, skin135}
     plane1 = { x = 500, y = 500, rot = 90, skin = skinPlane}
 
     --Variables globales
     delta_rot = 1.8
 
-    width = skin30:getWidth()
-    height = skin30:getHeight()
+    width = skin90:getWidth()
+    height = skin90:getHeight()
+
+    activeRays = {}
 end
 
 function love.draw()
@@ -26,11 +27,30 @@ function love.draw()
     love.graphics.print(delta_y, 350, 0)
 
     love.graphics.draw(actualSkin, plane1.x, plane1.y, -(plane1.rot-90)*0.1, 1, 1, width / 2, height / 2)    
+
+    love.graphics.print( table.maxn(activeRays), 0, 10)
+
+    drawRay = function(key, ray)
+        length = 20
+
+        delta_x = math.sin((ray.rot - 90) * 0.1)
+        delta_y = math.cos((ray.rot - 90) * 0.1)
+
+        x2 = ray.x - (delta_x * length)
+        y2 = ray.y - (delta_y * length)
+
+        love.graphics.setColor(255, 0, 0) 
+        love.graphics.setLine(2, "smooth")
+        love.graphics.line(ray.x, ray.y, x2, y2)
+    end
+
+    table.foreach(activeRays, drawRay)
+    love.graphics.reset()
 end
 
 function love.update()
 
-    actualSkin = plane1.skin[3]
+    actualSkin = plane1.skin[2]
     
     delta_x = math.sin((plane1.rot - 90) * 0.1)
     delta_y = math.cos((plane1.rot - 90) * 0.1)
@@ -39,12 +59,32 @@ function love.update()
     plane1.y = plane1.y - (delta_y * 10)
 
     if love.keyboard.isDown("a") then
-        plane1.rot = plane1.rot - delta_rot
-        actualSkin = plane1.skin[4]
+        plane1.rot = plane1.rot + delta_rot
+        actualSkin = plane1.skin[1]
     end
 
     if love.keyboard.isDown("d") then
-        plane1.rot = plane1.rot + delta_rot
-        actualSkin = plane1.skin[2]
+        plane1.rot = plane1.rot - delta_rot
+        actualSkin = plane1.skin[3]
     end
+
+    if love.keyboard.isDown("w") then
+        newRay = {x = plane1.x, y = plane1.y, rot = plane1.rot }
+        table.insert(activeRays, newRay)
+    end
+
+    updateRay = function(key, ray)
+        speed = 30
+
+        delta_x = math.sin((ray.rot - 90) * 0.1)
+        delta_y = math.cos((ray.rot - 90) * 0.1)
+
+        x2 = ray.x - (delta_x * speed)
+        y2 = ray.y - (delta_y * speed)
+
+        ray.x = x2
+        ray.y = y2
+    end
+
+    table.foreach(activeRays, updateRay)
 end
